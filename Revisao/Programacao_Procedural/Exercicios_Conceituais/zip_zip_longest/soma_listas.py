3e60b5a0fd33880887a9f5a1472038c6a788595d
Gabriel-Alexandre <biellalex86@gmail.com>
2021-11-19 10:35:18 -0300

    rev(exercício): zip + listcompresions

---
+++ Revisao/Programacao_Procedural/Exercicios_Conceituais/zip_zip_longest/soma_listas.py
@@ -0,0 +1,11 @@
+from itertools import zip_longest
+ 
+lista_a = [10, 2, 3, 4, 5]
+lista_b = [12, 2, 3, 6, 50, 60, 70]
+lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
+print(lista_soma)  # [22, 4, 6, 10, 55, 60, 70]
+
+# lista_a = [10, 2, 3, 4, 5]
+# lista_b = [12, 2, 3, 6, 50, 60, 70]
+# lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+# print(lista_soma)  # Saída: [22, 4, 6, 10, 55]
