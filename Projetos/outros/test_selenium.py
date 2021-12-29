1bdb87743356ebdb5cc66f926f598c830262c9b2
Gabriel-Alexandre <biellalex86@gmail.com>
2021-12-29 18:56:49 -0300

    navegacao - selenium

--- Projetos/outros/test_selenium.py
+++ Projetos/outros/test_selenium.py
@@ -1,20 +1,21 @@
 from selenium.webdriver import Firefox
 from time import sleep
 
-url = "https://www.busuu.com/pt/logout"
+url = 'https://www.busuu.com/pt/hello'
 
 browser = Firefox()
 
-print("Browser1: ", browser)
-
+# Faz requisição para a página.
 browser.get(url)
-a = browser.find_element_by_xpath('//*[@id="main-wrapper"]/div[1]/header/nav/div/div[1]/div[4]/a')
+# Encontra elemento e pega atributo.
+login = browser.find_element_by_xpath('/html/body/div[2]/div[1]/header/nav/div/div/div[2]/a').get_attribute('href')
+# Encontra elemento e clica.
+browser.find_element_by_xpath('/html/body/div[2]/div[1]/header/nav/div/div/div[2]/a').click()
+# Faz requisição para a próxima página.
+browser.get(login)
+# Escreve no input de login.
+browser.find_element_by_xpath('/html/body/div[2]/div[2]/div/section/div[1]/form/div[2]/div[1]/input').send_keys("IssoEhUmTeste")
 
-print("Browser1: ", browser)
-print("a: ", a)
-
-# a.find_element_by_xpath('//*[@id="google-authentication"]').click()
-
-# sleep(1)
+sleep(1)
 
 browser.quit()
